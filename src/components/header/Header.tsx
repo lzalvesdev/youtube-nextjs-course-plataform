35544abb8daa79acfa7b68e59a2be5408aecb2bd
Luiz Alves <luizha879@gmail.com>
2024-07-24 09:20:38 -0300

    feat: add responsive header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { usePathname } from "next/navigation";
 import Link from "next/link";
+import { MdMenu } from "react-icons/md";
 
 export const Header = () => {
   const currentPath = usePathname();
@@ -8,7 +9,11 @@ export const Header = () => {
   return(
     <>
      <header>
-      <nav className="flex items-center justify-center bg-primary py-4">
+      <nav className="flex items-center gap-6 justify-start md:justify-center bg-primary py-2 sm:py-4 px-6">
+        <button className="sm:hidden">
+          <MdMenu size={24}/>
+        </button>
+        
         <ul className="flex gap-4 items-center">
         <li className="my-2">
             <Link href='/' className="border-2 rounded-md py-2 px-1 font-bold">
@@ -16,17 +21,19 @@ export const Header = () => {
             </Link>
           </li>
           
-          <li>
+          <li className="hidden sm:block">
           <Link href='/' data-active={currentPath === '/'} className='data-[active=true]:underline'>
             Página inicial
           </Link>
         </li>
-        <li>
+        <li className="hidden sm:block">
           <Link href='/cursos' data-active={currentPath === '/cursos'} className='data-[active=true]:underline'>
             Cursos
           </Link>
         </li>
         </ul>
+
+        <h1 className="sm:hidden">EducaNet - Página inicial</h1>
       </nav>
 
      </header>
