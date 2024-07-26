8011580473835548c57900f87b31cfac868c160b
lzalvesdev <luizha879@gmail.com>
2024-07-26 18:49:20 -0300

    feat: add navigation drawer

--- src/app/cursos/page.tsx
+++ src/app/cursos/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "EducaNet - Todos os cursos",
+};
+
 export default function PageCursos() {
   return(
     <main>
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,41 +2,77 @@
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { MdMenu } from "react-icons/md";
+import { useEffect, useState } from "react";
 
 export const Header = () => {
+  const [title, setTitle] = useState('EducaNet');
+  const [drawer, setDrawer] = useState(false);
+
   const currentPath = usePathname();
 
-  return(
+  useEffect(() => {
+    setTitle(document.title);
+    setDrawer(false);
+  }, [currentPath]);
+
+  return (
     <>
-     <header>
-      <nav className="flex items-center gap-6 justify-start md:justify-center bg-primary py-2 sm:py-4 px-6">
-        <button className="sm:hidden">
-          <MdMenu size={24}/>
-        </button>
-        
-        <ul className="flex gap-4 items-center">
-        <li className="my-2">
-            <Link href='/' className="border-2 rounded-md py-2 px-1 font-bold">
-            EDUCANET
-            </Link>
-          </li>
-          
-          <li className="hidden sm:block">
-          <Link href='/' data-active={currentPath === '/'} className='data-[active=true]:underline'>
-            Página inicial
-          </Link>
-        </li>
-        <li className="hidden sm:block">
-          <Link href='/cursos' data-active={currentPath === '/cursos'} className='data-[active=true]:underline'>
-            Cursos
-          </Link>
-        </li>
-        </ul>
+      <header>
+        <nav className="flex items-center gap-6 justify-start md:justify-center bg-primary py-2 sm:py-4 px-6">
+          <button className="sm:hidden" onClick={() => setDrawer(true)}>
+            <MdMenu size={24} />
+          </button>
 
-        <h1 className="sm:hidden">EducaNet - Página inicial</h1>
-      </nav>
+          <ul
+            tabIndex={drawer ? -1 : undefined}
+            className="flex gap-4 items-center">
+            <li className="my-2">
+              <Link href='/' className="border-2 rounded-md py-2 px-1 font-bold">
+                EDUCANET
+              </Link>
+            </li>
 
-     </header>
+            <li className="hidden sm:block">
+              <Link href='/' data-active={currentPath === '/'} className='data-[active=true]:underline'>
+                Página inicial
+              </Link>
+            </li>
+            <li className="hidden sm:block">
+              <Link href='/cursos' data-active={currentPath === '/cursos'} className='data-[active=true]:underline'>
+                Cursos
+              </Link>
+            </li>
+          </ul>
+
+          {/* Burguer menu */}
+
+          <div
+            data-open={drawer}
+            tabIndex={drawer ? 0 : -1}
+            onClick={() => setDrawer(false)}
+            className="fixed top-0 left-0 bottom-0 bg-gradient-to-r from-background right-0 transition-transform duration-360 ease-in-out data-[open=false]:-translate-x-full"
+          >
+            <ul className="flex gap-4 flex-col p-4 w-60 h-full bg-background" onClick={(e) => e.stopPropagation()}>
+              <li className="border-b border-white">
+                <Link href='/' className=''>
+                  Página inicial
+                </Link>
+              </li>
+
+              <li className="">
+                <Link href='/cursos' data-active={currentPath === '/cursos'} className='data-[active=true]:underline'>
+                  Cursos
+                </Link>
+              </li>
+            </ul>
+          </div>
+
+          <h1 className="sm:hidden">
+            {title}
+          </h1>
+        </nav>
+
+      </header>
     </>
   );
 };
