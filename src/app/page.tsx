c6c52dc7d97edfb92bb8c624a8eee3c6d0548865
Luiz Alves <luizha879@gmail.com>
2024-08-03 11:47:44 -0300

    feat: add props to card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,12 @@ import { Card } from "@/components/card/Card";
 export default function PageHome() {
   return (
     <main className="bg-background">
-      <Card />
+      <Card
+        href="/cusos/444"
+        image="https://i.ytimg.com/vi/bP47qRVRqQs/hqdefault.jpg"
+        title="Titulo"
+        description={`Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus quasi explicabo temporibus amet dolorum officiis numquam, pariatur iure delectus totam aliquid eveniet accusamus fuga modi consequatur dolores sit nulla excepturi!`}
+      />
     </main>
   );
 }
--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,21 +1,28 @@
 import Image from "next/image";
 import Link from "next/link";
 
-export const Card = (id: any) => {
+interface ICardProps {
+  href: string;
+  image: string;
+  title: string;
+  description: string;
+}
+
+export const Card = ({ href, image, title, description }: ICardProps) => {
   return (
-    <Link href={`/cursos/454`} className="sm:hover:no-underline">
+    <Link href={href} className="sm:hover:no-underline">
       <article className="flex gap-4 flex-col p-2 rounded sm:hover:bg-primary">
         <Image
           className="aspect-video object-cover rounded-3xl"
           width={1000}
           height={0}
           draggable={false}
-          alt={"Curso"}
-          src={"https://i.ytimg.com/vi/bP47qRVRqQs/hqdefault.jpg"}
+          alt={title}
+          src={image}
         />
 
         <h4 className="font-extrabold text-lg">Titulo</h4>
-        <p className="line-clamp-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus quasi explicabo temporibus amet dolorum officiis numquam, pariatur iure delectus totam aliquid eveniet accusamus fuga modi consequatur dolores sit nulla excepturi!</p>
+        <p className="line-clamp-3">{description}</p>
       </article>
     </Link>
   );
